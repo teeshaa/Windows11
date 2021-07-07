eca56123287735490216963e6e449c276f217cce
Tisha Ghevariya <74093982+tishhaa@users.noreply.github.com>
2021-07-07 20:10:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+// Taskbar icons Transition
+// First Icon
+let starticon = document.getElementsByClassName("starticon")[0]
+let startmenu = document.getElementsByClassName("startmenu")[0]
+
+starticon.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(startmenu.style.bottom == "50px"){
+        startmenu.style.bottom = "-655px"
+    }
+    else{
+        startmenu.style.bottom = "50px"
+    }
+})
+
+// Second Icon
+
+let searchicon = document.getElementsByClassName("searchicon")[0]
+let searchin = document.getElementsByClassName("searchin")[0]
+
+searchicon.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(searchin.style.bottom == "50px"){
+        searchin.style.bottom = "-655px"
+    }
+    else{
+        searchin.style.bottom = "50px"
+    }
+})
+
+// Third Icon
+let thirdicon = document.getElementsByClassName("thirdicon")[0]
+let threein = document.getElementsByClassName("threein")[0]
+
+thirdicon.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(threein.style.bottom == "50px"){
+        threein.style.bottom = "-800px"
+    }
+    else{
+        threein.style.bottom = "50px"
+    }
+})
+
+// Fourth Icon
+
+let fourthicon = document.getElementsByClassName("fourthicon")[0]
+let fourin = document.getElementsByClassName("fourin")[0]
+
+fourthicon.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(fourin.style.bottom == "50px"){
+        fourin.style.bottom = "-800px"
+    }
+    else{
+        fourin.style.bottom = "50px"
+    }
+})
+
+// Fifth Icon
+
+
+let mcicon = document.getElementsByClassName("mcicon")[0]
+let mcin = document.getElementsByClassName("mcin")[0]
+
+mcicon.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(mcin.style.bottom == "50px"){
+        mcin.style.bottom = "-800px"
+    }
+    else{
+        mcin.style.bottom = "50px"
+    }
+})
+
+// Sixth Icon
+
+let vscodeicon = document.getElementsByClassName("vscodeicon")[0]
+let vscodein = document.getElementsByClassName("vscodein")[0]
+
+vscodeicon.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(vscodein.style.bottom == "50px"){
+        vscodein.style.bottom = "-800px"
+    }
+    else{
+        vscodein.style.bottom = "50px"
+    }
+})
+
+// Seventh Icon
+
+let chromeicon = document.getElementsByClassName("chromeicon")[0]
+let chromein = document.getElementsByClassName("chromein")[0]
+
+chromeicon.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(chromein.style.bottom == "50px"){
+        chromein.style.bottom = "-800px"
+    }
+    else{
+        chromein.style.bottom = "50px"
+    }
+})
+
+// Eighth Icon
+
+let foldericon = document.getElementsByClassName("foldericon")[0]
+let folderin = document.getElementsByClassName("folderin")[0]
+
+foldericon.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(folderin.style.bottom == "50px"){
+        folderin.style.bottom = "-800px"
+    }
+    else{
+        folderin.style.bottom = "50px"
+    }
+})
+
+
+
